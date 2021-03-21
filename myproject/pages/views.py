a6f885517e6275a6b752254159a4edf9786f20e0
Borko <bstoilov81@gmail.com>
2021-03-21 20:03:38 +0200

    finihs for today

--- myproject/pages/views.py
+++ myproject/pages/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
     print(request.user)
     # return HttpResponse("<h1>Home view page</h1>")
-    return render(request, 'pages/home.html', {})
+    return render(request, 'pages/home.html', {'user': request.user})
 
 
 def contact_view(request, *args, **kwargs):
