2518a79162d1cf03f463f5778f0d6dad35ecfa20
Borko <bstoilov81@gmail.com>
2021-03-25 21:52:01 +0200

    Add widgets to pure forms

--- myproject/products/forms.py
+++ myproject/products/forms.py
@@ -10,6 +10,13 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(label="New Title", widget=forms.TextInput(attrs={
+        "placeholder": "Enter title here"
+    }))
+    description = forms.CharField(label="New Description", widget=forms.Textarea(attrs={
+        "placeholder": "Description",
+        "class": "new-class-name",
+        "rows": 10,
+        "cols": 30,
+    }))
+    price = forms.DecimalField(label="New price", initial=0.99)
