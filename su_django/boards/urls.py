b3169491bada2595a4eb018eb4891e7922e3523a
Borko <bstoilov81@gmail.com>
2021-05-23 12:57:40 +0300

    Crud in book

--- su_django/boards/urls.py
+++ su_django/boards/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, show_books
+from .views import home, show_books, upload_book, edit_book, delete_book
 
 urlpatterns = [
     path('', home, name='home'),
     path('all/', show_books, name='all_book'),
+    path('new/', upload_book, name='upload_book'),
+    path('edit/<int:book_id>/book/',edit_book, name='edit_book'),
+    path('delete/<int:book_id>/book', delete_book, name='delete_book'),
 ]
--- su_django/boards/views.py
+++ su_django/boards/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+from .forms import BookCreate
 from .models import Board, Book
 
 
@@ -14,3 +16,39 @@ def show_books(request):
         'shelf': shelf
     }
     return render(request, 'boards/all_books.html', content)
+
+
+def upload_book(request):
+    forms = BookCreate()
+    if request.method == 'POST':
+        forms = BookCreate(request.POST, request.FILES)
+        if forms.is_valid():
+            forms.save()
+            return redirect('all_book')
+        else:
+            return HttpResponse('Not valid form reload in <a href={{ url: "index" }}>reload</a>')
+    return render(request, 'boards/new_book.html', {'forms': forms})
+
+
+def edit_book(request, book_id):
+    book_id = int(book_id)
+    try:
+        book_sel = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        return redirect('all_book')
+    book_form = BookCreate(request.POST or None, instance=book_sel)
+    if book_form.is_valid():
+        book_form.save()
+        return redirect('all_book')
+    return render(request, 'boards/new_book.html', {'forms': book_form})
+
+
+def delete_book(request, book_id):
+    book_id = int(book_id)
+    try:
+        book_sel = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        return redirect('all_book')
+    book_sel.delete()
+    return redirect('all_book')
+
