c928d55ecc1ded76dbb9d77a3f6f7b26ca7cd13b
Borko <bstoilov81@gmail.com>
2021-06-04 19:25:11 +0300

    Add ajax and some minimal in views

--- todoapp/app/urls.py
+++ todoapp/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, create_new, change_done
 
 
-app_name = 'app'
 urlpatterns = [
     path('', index, name='index'),
+    path('create_new/', create_new, name='create_new'),
+    path('change_done/', change_done, name='change_done'),
 ]
--- todoapp/app/views.py
+++ todoapp/app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
 from datetime import datetime
 
-from .models import Todo
+from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import Todo
 
 
 def index(request):
@@ -13,3 +13,24 @@ def index(request):
         'date': datetime.now()
     }
     return render(request, 'app/index.html', context)
+
+
+def create_new(request):
+    if request.method == 'POST':
+        print('-' * 100)
+        print(request)
+        print('-' * 100)
+        data = {}
+        data['title'] = request.POST.get('title', None)
+        data['description'] = request.POST.get('description', None)
+        Todo.objects.create(title=data['title'], description=data['description'])
+        return HttpResponse("Successfully added new todo")
+    return HttpResponse('Not allowed method')
+
+
+def change_done(request):
+    id = request.GET.get('id')
+    dat = Todo.objects.get(id=id)
+    dat.is_done = not dat.is_done
+    dat.save()
+    return HttpResponse('Change Done function')
