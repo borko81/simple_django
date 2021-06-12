de9298cc5f54247c7bba86bfc4375f1f950b46cd
Borko <bstoilov81@gmail.com>
2021-06-12 16:52:07 +0300

    Create and view def

--- su_django/forms_lab/forms.py
+++ su_django/forms_lab/forms.py
@@ -1,11 +1,26 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+
+def check_name_length(value):
+    if not value[0] == value[0].upper():
+        raise ValidationError('Name must be start with uppercase')
+    if not len(value) > 3:
+        raise ValidationError('Name must be big from three symbol')
+
+
+def validate_age_is_bigger_from_zero(value):
+    if value <= 17:
+        raise ValidationError('Age must be bigger from 18')
 
 class MyForm(forms.Form):
     name = forms.CharField(
+        validators=[check_name_length],
         widget=forms.TextInput()
     )
-    age = forms.IntegerField()
+    age = forms.IntegerField(
+        validators=[validate_age_is_bigger_from_zero],
+    )
     email = forms.EmailField()
     password = forms.CharField(
         widget=forms.PasswordInput()
@@ -13,3 +28,4 @@ class MyForm(forms.Form):
     text = forms.CharField(
         widget=forms.Textarea()
     )
+
